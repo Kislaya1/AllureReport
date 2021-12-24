112356c7f6d3fa8b3b1ddf2b48dcfea2d94f020d
Kislaya1 <kislayapant@gmail.com>
2021-12-24 12:42:48 +0530

    Updated Projects

--- src/test/java/com/allure/report/AllureTest.java
+++ src/test/java/com/allure/report/AllureTest.java
@@ -1,13 +1,19 @@
 package com.allure.report;
 
+import io.qameta.allure.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.annotations.Test;
 
+@Epic("Allure Report")
+@Feature("Allure Test")
 public class AllureTest {
   private static final Logger LOGGER = LoggerFactory.getLogger(AllureTest.class.getName());
 
   @Test
+  @Severity(SeverityLevel.BLOCKER)
+  @Description("Test Description")
+  @Story("Story Description")
   public void test1() throws Exception {
     LOGGER.info("My Test 1");
   }
