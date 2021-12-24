7004275394653766da675cd61e572b23a70dcbf1
Kislaya1 <kislayapant@gmail.com>
2021-12-24 15:47:24 +0530

    Updated Project

--- src/test/java/com/allure/report/AllureTest.java
+++ src/test/java/com/allure/report/AllureTest.java
@@ -3,6 +3,7 @@ package com.allure.report;
 import io.qameta.allure.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 @Epic("Allure Report")
@@ -11,10 +12,132 @@ public class AllureTest {
   private static final Logger LOGGER = LoggerFactory.getLogger(AllureTest.class.getName());
 
   @Test
+  @Owner("Kislaya")
   @Severity(SeverityLevel.BLOCKER)
   @Description("Test Description")
   @Story("Story Description")
   public void test1() throws Exception {
+    Assert.assertTrue(true);
+    LOGGER.info("My Test 1");
+  }
+
+  @Test
+  @Owner("Kislaya")
+  @Severity(SeverityLevel.BLOCKER)
+  @Description("Test Description")
+  @Story("Story Description")
+  public void test2() throws Exception {
+    Assert.assertTrue(true);
+    LOGGER.info("My Test 1");
+  }
+
+  @Test
+  @Owner("Kislaya")
+  @Severity(SeverityLevel.BLOCKER)
+  @Description("Test Description")
+  @Story("Story Description")
+  public void test3() throws Exception {
+    Assert.assertTrue(true);
+    LOGGER.info("My Test 1");
+  }
+
+  @Test
+  @Owner("Kislaya")
+  @Severity(SeverityLevel.BLOCKER)
+  @Description("Test Description")
+  @Story("Story Description")
+  public void test4() throws Exception {
+    Assert.assertTrue(true);
+    LOGGER.info("My Test 1");
+  }
+
+  @Test
+  @Owner("Kislaya")
+  @Severity(SeverityLevel.BLOCKER)
+  @Description("Test Description")
+  @Story("Story Description")
+  public void test5() throws Exception {
+    Assert.assertTrue(true);
+    LOGGER.info("My Test 1");
+  }
+
+  @Test
+  @Owner("Kislaya")
+  @Severity(SeverityLevel.BLOCKER)
+  @Description("Test Description")
+  @Story("Story Description")
+  public void test6() throws Exception {
+    Assert.assertTrue(true);
+    LOGGER.info("My Test 1");
+  }
+
+  @Test
+  @Owner("Kislaya")
+  @Severity(SeverityLevel.BLOCKER)
+  @Description("Test Description")
+  @Story("Story Description")
+  public void test7() throws Exception {
+    Assert.assertTrue(true);
+    LOGGER.info("My Test 1");
+  }
+
+  @Test
+  @Owner("Kislaya")
+  @Severity(SeverityLevel.BLOCKER)
+  @Description("Test Description")
+  @Story("Story Description")
+  public void test8() throws Exception {
+    Assert.assertTrue(true);
+    LOGGER.info("My Test 1");
+  }
+
+  @Test
+  @Owner("Kislaya")
+  @Severity(SeverityLevel.BLOCKER)
+  @Description("Test Description")
+  @Story("Story Description")
+  public void test9() throws Exception {
+    Assert.assertTrue(true);
+    LOGGER.info("My Test 1");
+  }
+
+  @Test
+  @Owner("Kislaya")
+  @Severity(SeverityLevel.BLOCKER)
+  @Description("Test Description")
+  @Story("Story Description")
+  public void test10() throws Exception {
+    Assert.assertTrue(true);
+    LOGGER.info("My Test 1");
+  }
+
+  @Test
+  @Owner("Kislaya")
+  @Severity(SeverityLevel.BLOCKER)
+  @Description("Test Description")
+  @Story("Story Description")
+  public void test11() throws Exception {
+    Assert.assertTrue(true);
+    LOGGER.info("My Test 1");
+  }
+
+  @Test
+  @Owner("Kislaya")
+  @Severity(SeverityLevel.BLOCKER)
+  @Description("Test Description")
+  @Story("Story Description")
+  public void test12() throws Exception {
+    Assert.assertTrue(true);
+    LOGGER.info("My Test 1");
+  }
+
+  @Test
+  @Owner("Kislaya")
+  @Severity(SeverityLevel.BLOCKER)
+  @Description("Test Description")
+  @Story("Story Description")
+  public void test13() throws Exception {
+    Assert.assertTrue(true);
     LOGGER.info("My Test 1");
   }
 }
